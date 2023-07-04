4b548f355dc70c059899d5fcf95f3f6976da2336
Jamshid <murodovjamshid347@gmail.com>
2023-07-04 20:35:02 +0500

    first

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let box = 1
-for (let i =1; i <=5; i++) {
-    box= box * i
-
-    console.log(box);
+var son = +prompt( "Son kiriting:")
+while (isNaN(son) || son == 0  ) {
+    son = +prompt("Bu son emas Iltimos son kriting:")
 }
+
+var daraja = +prompt( "Daraja kiriting:")
+while (isNaN(daraja) || daraja == 0  ) {
+    daraja = +prompt("Bu daraja emas Iltimos daraja kriting:")
+}
+
+let box =1
+for (let   i=0;  i<daraja && son; i++) {
+  box *= son
+  console.log(box);
+}
+alert( "Javob " + box )
+alert("Tashakkur siz to'g'ri javob berdingiz")
+
+
+
+  
